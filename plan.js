aaff9c092371a9b1355e3d5807f9b1c787886622
Lidan Zhang <zhanglidan2002@gmail.com>
2024-01-25 18:52:33 +0100

    modif page about us

--- plan.js
+++ plan.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
 
-    function initMap() {
-        var mapProp= {
-          center: new google.maps.LatLng(51.508742,-0.120850),
-          zoom:5,
-        };
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-      }
-   
+    
 });
